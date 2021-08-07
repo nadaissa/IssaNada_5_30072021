97e9969748669d0eaae2b65cd87137a60518c651
Nada <rosariojesus909@gmail.com>
2021-08-07 22:51:59 +0200

    [Build] :construction: :sparkles: :boom: first styling rules with DOM

---
+++ js/main.js
@@ -0,0 +1,38 @@
+//common display rules
+
+//main-wrapper and general content style
+document.getElementById("main-wrapper").style.cssText = 
+"overflow: hidden; margin: 0; background: #2E2E2E; padding: 15px; font-family: 'Georama', sans-serif";
+
+    //header style
+    document.getElementById("header").style.cssText = 
+    "border-bottom: solid #E78B06; margin-bottom: 35px; padding-bottom: 0px";
+
+        //logo icon size and margins
+        document.getElementById("nav__logo-icon").style.cssText =
+        "width: 1.5 rem; height: 1.5rem; margin-bottom: 10px"
+
+        document.getElementById("nav__logo-link").style.cssText =
+        "color: #8F5BFE; font-weight: bold";
+
+        document.getElementById("nav__home-icon").style.color = "#E78B06";
+        document.getElementById("nav__cart-icon").style.color = "#E78B06";
+
+    //intro style
+    document.getElementById("intro__jumbotron").style.cssText =
+    "max-width: 550px; padding: 25px 15px 25px 15px; background: linear-gradient(180deg, rgba(231,139,6,0.9) 14%, rgba(143,91,254,0.8) 100%); font-size: 0.8rem; color: #ffffff"
+
+        document.getElementById("intro__h1").style.cssText = 
+        "font-size: 1.2rem; font-weight: bold; margin-top: 10px;";
+
+    //temporary content
+    document.getElementById("pcontent").style.color = "white";
+
+    //footer style
+    document.getElementById("footer").style.cssText = 
+    "border-top: solid #8F5BFE; margin-top: 15px; padding-top: 20px; margin-bottom: 0px";
+        document.getElementById("about-link").style.color = "#fff";
+        document.getElementById("partners-link").style.color = "#fff";
+        document.getElementById("contact-link").style.color = "#fff";
+
+//end of common display rules
