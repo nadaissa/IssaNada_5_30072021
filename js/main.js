4db7916c24a828c69f740c621819d77fd51768dd
Nada <rosariojesus909@gmail.com>
2021-08-10 11:26:01 +0200

    [Build] :construction: :sparkles: first improvements on DOM styling rules

--- js/main.js
+++ js/main.js
@@ -15,24 +15,31 @@ document.getElementById("main-wrapper").style.cssText =
         document.getElementById("nav__logo-link").style.cssText =
         "color: #8F5BFE; font-weight: bold";
 
-        document.getElementById("nav__home-icon").style.color = "#E78B06";
-        document.getElementById("nav__cart-icon").style.color = "#E78B06";
+        let navLinks = document.getElementsByClassName("nav-link");
+            for(i of navLinks){
+                i.style.color="#E78B06";
+            };
 
     //intro style
     document.getElementById("intro__jumbotron").style.cssText =
-    "max-width: 550px; padding: 25px 15px 25px 15px; background: linear-gradient(180deg, rgba(231,139,6,0.9) 14%, rgba(143,91,254,0.8) 100%); font-size: 0.8rem; color: #ffffff"
-
-        document.getElementById("intro__h1").style.cssText = 
-        "font-size: 1.2rem; font-weight: bold; margin-top: 10px;";
+    "max-width: 550px; padding: 25px 15px 25px 15px; background: linear-gradient(180deg, rgba(231,139,6,0.9) 14%, rgba(143,91,254,0.8) 100%); font-size: 0.8rem; color: #ffffff";
+        
+        let heading1 = document.getElementsByTagName("h1");
+            for(i of heading1){
+                i.style.cssText="font-size: 1.2rem; font-weight: bold; margin-top: 10px;"
+            };
 
     //temporary content
     document.getElementById("pcontent").style.color = "white";
 
     //footer style
-    document.getElementById("footer").style.cssText = 
+    document.getElementById('footer').style.cssText = 
     "border-top: solid #8F5BFE; margin-top: 15px; padding-top: 20px; margin-bottom: 0px";
-        document.getElementById("about-link").style.color = "#fff";
-        document.getElementById("partners-link").style.color = "#fff";
-        document.getElementById("contact-link").style.color = "#fff";
+        
+        let footer = document.querySelector('footer');
+            let footerLinks = footer.querySelectorAll('a');
+                for(let i=0;i<footerLinks.length;i++){
+                    footerLinks[i].style.color="#fff";
+                };
 
 //end of common display rules
