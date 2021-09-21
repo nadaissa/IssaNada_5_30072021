08168384b8e6ba26b8e5f59cf92dec614933aaf1
Nada <rosariojesus909@gmail.com>
2021-09-21 15:39:09 +0200

    [JS] :bulb: added comments to product class

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,14 @@
-//Product class creation to be used on the main page cards display
+//Product class creation to be used on the main page cards display, product page and cart page. 
+//! not all of the constructor elements are present on main or product page. 
+//I used the same spelling used in the backend files and added two other elements (option and quantity)
 class Product{
     constructor(id, name, description, imageUrl, price, option, quantity) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.imageUrl = imageUrl;
-        this.price = +price;
+        this.price = price;
         this.option = option;
-        this.quantity = +quantity;        
+        this.quantity = quantity;        
     };
 };
