8ba1655acb357148895a40630029749b08b87434
Nada <rosariojesus909@gmail.com>
2021-09-24 18:34:44 +0200

    [Build] :alembic: failed experiments of js on product page

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,14 @@
 //Product class creation to be used on the main page cards display, product page and cart page. 
 //! not all of the constructor elements are present on main or product page. 
-//I used the same spelling used in the backend files and added two other elements (option and quantity)
+//I used the same spelling used in the backend files and added two other elements (variation and quantity)
 class Product{
-    constructor(id, name, description, imageUrl, price, option, quantity) {
+    constructor(id, name, description, imageUrl, price, variation, quantity) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.imageUrl = imageUrl;
         this.price = price;
-        this.option = option;
+        this.variation = variation;
         this.quantity = quantity;        
     };
 };
--- js/main.js
+++ js/main.js
@@ -6,11 +6,14 @@ fetch("http://localhost:3000/api/cameras")
         ListProducts(data);
     })
 
+    //the error catch
     .catch(function(error) {
-        console.log('Il y a eu un problème: ' + error.message); //the error catch needs to be confirmed
+        console.log('Il y a eu un problème: ' + error.message);
     });
 
     //The function mentioned in the fetch needs to be configured in order to display the products
+    //image, title and description are called directly in the data fetched previously and then in relation to the product class i've created
+    //the link to the product page includes the query http protocole with reference to the product id
     function ListProducts(data) {
         for (product of data){
             const content = document.querySelector("#pcontent");
@@ -21,7 +24,7 @@ fetch("http://localhost:3000/api/cameras")
                         <div class="card-body">
                             <h5 class="card-title font-weight-bold">${product.name}</h5>
                             <p class="card-text">${product.description}</p>
-                            <a href="#" class="btn btn-primary btn-dark font-weight-bold ">Voir le produit</a>
+                            <a href="./product.html?_id=${product._id}" class="btn btn-primary btn-dark font-weight-bold ">Voir le produit</a>
                         </div>
                 </div>
             </div>`
