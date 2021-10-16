d5520191c4812dcd438e0e80ca6e6e0da1c7dc1d
Nada <rosariojesus909@gmail.com>
2021-10-16 22:06:38 +0200

    [JS] :bulb: added comments to regex elements

--- front-end/js/cart.js
+++ front-end/js/cart.js
@@ -14,19 +14,28 @@ function productsDisplay(product) {
 
 
 //setting input rules for form validation
-//setting constants for the form div and the submit button
+
+//setting constants for the form div, the submit button and the checkbox
 const orderForm = document.querySelector("#orderForm");
 const submitForm = document.querySelector("#submitBtn");
 const checkBox = document.querySelector("#checkbox");
+
 //setting constants for the regex conditions
+/*for names one or more strings from characters between a and z (lower and upper cases) 
+with possible accent letters with one white space or - only, 
+or one or more strings with no space or -*/
 const condName = /^(([a-zA-ZÀ-ÿ]+[\s\-]{1}[a-zA-ZÀ-ÿ]+)|([a-zA-ZÀ-ÿ]+))$/;
+/*for emails one or more strings from characters between a and z (lower and upper cases) 
+and/or entire number and/or dots dashes then @ symbole, then two or more characters 
+with the same rules with no special characters then a dot and then lowercases between 2 and 4 letters*/
 const condMail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$/;
+//for city name same as names but limited to 10 characters
 const condCity = /^(([a-zA-ZÀ-ÿ]+[\s\-]{1}[a-zA-ZÀ-ÿ]+)|([a-zA-ZÀ-ÿ]+)){1,10}$/;
+/*for zip code a non-capturing group of 5digits, first two ange from 01 to 98 
+(\d indicates for each of this cases we have only one digit) then the rest is limited to 3 digits*/
 const condZip = /^(?:[0-8]\d|9[0-8])\d{3}$/;
+//for address same as names but with possibility of numbers and limited to 10 characters by string
 const condAddress = /^(([a-zA-ZÀ-ÿ0-9]+[\s\-]{1}[a-zA-ZÀ-ÿ0-9]+)){1,10}$/;
-
-      
-
 
 
     //setting the event listener in case of click on the submit button
@@ -36,6 +45,7 @@ const condAddress = /^(([a-zA-ZÀ-ÿ0-9]+[\s\-]{1}[a-zA-ZÀ-ÿ0-9]+)){1,10}$/;
     });
     
     function checkInput (){
+        //setting the contactInfo object to use later in the confirmation page
         const contactInfo = {
             firstName: document.querySelector("#firstName").value,
             lastName: document.querySelector("#lastName").value,
@@ -45,7 +55,7 @@ const condAddress = /^(([a-zA-ZÀ-ÿ0-9]+[\s\-]{1}[a-zA-ZÀ-ÿ0-9]+)){1,10}$/;
             inputZip: document.querySelector("#inputZip").value,
         };
 
-        
+        //setting the loop for the successful input
         if (
             (condName.test(contactInfo.firstName) === true) &
             (condName.test(contactInfo.lastName) === true) &
@@ -58,14 +68,17 @@ const condAddress = /^(([a-zA-ZÀ-ÿ0-9]+[\s\-]{1}[a-zA-ZÀ-ÿ0-9]+)){1,10}$/;
                 //alert("bravo!");
                 submitOk();
 
+        //setting the loop for the non-successful input
         } else {   
 
             alert("Merci de respecter les consignes de saisie et de remplir et cocher tous les champs!");
         }
     };        
             
-        function submitOk(){
-            
+        
+    //setting the function for the validation upcome
+    function submitOk(){
+
             console.log("mm");
             /*ici mettre function de submit ou il y a la list de produit et aussi la liste des contacts et post et définir le post
             //here create product list
