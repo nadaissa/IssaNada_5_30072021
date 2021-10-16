92261a1ee9cd9047e2baefcfc591120023fb82da
Nada <rosariojesus909@gmail.com>
2021-10-16 19:20:06 +0200

    [JS/HTML] :alembic: initail functionnal form alerts and loops - still need the post fetch

--- front-end/js/cart.js
+++ front-end/js/cart.js
@@ -11,81 +11,95 @@ function productsDisplay(product) {
     ` 
 };
 
+
+
+//setting input rules for form validation
+//setting constants for the form div and the submit button
 const orderForm = document.querySelector("#orderForm");
-orderForm.addEventListener("click", validate);
-function validate() {      
-    if( document.formArea.fName.value === "" ) {
-       alert( "Please provide your name!" );
-       document.formArea.fName.focus() ;
-       return false;
-    };
-    return( true );
-};
-
-//forms input rules
-//defining the form fields constants
-/*const orderForm = document.getElementsByid("orderForm");
-const firstName = document.getElementById("firstName");
-const lastName = document.getElementById("lastName");
-const email = document.getElementById("inputEmail4");
-const address = document.getElementById("inputAddress");
-const city = document.getElementById("inputCity");
-const zipCode = document.getElementById("inputZip");
-const checkBox = document.getElementById("invalidCheck2");
-
-//defining the form input conditions
+const submitForm = document.querySelector("#submitBtn");
+const checkBox = document.querySelector("#checkbox");
+//setting constants for the regex conditions
 const condName = /^(([a-zA-ZÀ-ÿ]+[\s\-]{1}[a-zA-ZÀ-ÿ]+)|([a-zA-ZÀ-ÿ]+))$/;
 const condMail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$/;
 const condCity = /^(([a-zA-ZÀ-ÿ]+[\s\-]{1}[a-zA-ZÀ-ÿ]+)|([a-zA-ZÀ-ÿ]+)){1,10}$/;
 const condZip = /^(?:[0-8]\d|9[0-8])\d{3}$/;
-const condAddress = /^(([a-zA-ZÀ-ÿ0-9]+[\s\-]{1}[a-zA-ZÀ-ÿ0-9]+)){1,10}$/;*/
+const condAddress = /^(([a-zA-ZÀ-ÿ0-9]+[\s\-]{1}[a-zA-ZÀ-ÿ0-9]+)){1,10}$/;
+
+      
 
 
-/*submitOrder.addEventListener("click", (event) => {
-    // contactInfo object
-    let contactInfo = {
-        firstName: document.getElementById("firstName").value,
-        lastName: document.getElementById("lastName").value,
-        email: document.getElementById("inputEmail4").value,
-        address: document.getElementById("inputAddress").value,
-        city: document.getElementById("inputCity").value,
-        zipCode: document.getElementById("inputZip").value,
+
+    //setting the event listener in case of click on the submit button
+    submitForm.addEventListener("click", (validate) =>{
+        validate.preventDefault();
+        checkInput();
+    });
+    
+    function checkInput (){
+        const contactInfo = {
+            firstName: document.querySelector("#firstName").value,
+            lastName: document.querySelector("#lastName").value,
+            inputEmail: document.querySelector("#inputEmail").value,
+            inputAddress: document.querySelector("#inputAddress").value,
+            inputCity: document.querySelector("#inputCity").value,
+            inputZip: document.querySelector("#inputZip").value,
+        };
+
         
-    }
-    // input conditions
-    if (        
-        (condName.test(contactInfo.firstName) == true) &
-        (condName.test(contactInfo.lastName) == true) &
-        (condMail.test(contactInfo.email) == true) &
-        (condAddress.test(contactInfo.address) == true) &
-        (condCity.test(contactInfo.city) == true) &
-        (condZip.test(contactInfo.zipCode) == true) &        
-        (checkBox.checked == true)
-    ) {
-        event.preventDefault();
+        if (
+            (condName.test(contactInfo.firstName) === true) &
+            (condName.test(contactInfo.lastName) === true) &
+            (condMail.test(contactInfo.inputEmail) === true) &
+            (condCity.test(contactInfo.inputCity) === true) &
+            (condZip.test(contactInfo.inputZip) === true) &
+            (condAddress.test(contactInfo.inputAddress) === true) &
+            (checkBox.checked === true) 
+            ) {
+                alert("bravo!");
 
-        // POST
-        fetch("https://teddies-api.herokuapp.com/api/cameras/order", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ contactInfo }),
-        });
-            .then((response) => response.json())
-            .then((data) => {
-                localStorage.setItem("order", JSON.stringify(data));
-                document.location.href = "order.html";
+        } else if(
+            (!condName.test(orderForm.firstName.value)) ||
+            (!condName.test(orderForm.lastName)) ||
+            (!condMail.test(contactInfo.inputEmail)) ||
+            (!condCity.test(contactInfo.inputCity)) ||
+            (!condZip.test(contactInfo.inputZip)) ||
+            (!condAddress.test(contactInfo.inputAddress)) ||
+            (checkBox.checked !== true)     
+
+        ) {
+            
+            alert("Merci de remplir ou cocher tous les champs!");    
+        
+        
+        } else {
+        
+            alert("Un problème inattendu!");
+        }
+    };        
+            
+            /*ici mettre function de submit ou il y a la list de produit et aussi la liste des contacts et post et définir le post
+            //here create product list
+            /*let products = [];
+            for (listId of basket) {
+                products.push(listId.id);
+            }
+
+            //here the post method
+            fetch("http://localhost:3000/api/cameras/order", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(contactInfo),
             });
-            .catch((erreur) => console.log("erreur : " + erreur));
-    } else {
-        alert(
-            "Veuillez remplir le formulaire pour valider votre commande."
-        );
-    };
-};*/
+                .then((response) => response.json())
+                .then((data) => {
+                    localStorage.setItem("order", JSON.stringify(data));
+                    document.location.href = "./confirmation.html";
+                });
+                .catch((erreur) => console.log("erreur : " + erreur));*/
 
 
-
+/*        */
 
 
